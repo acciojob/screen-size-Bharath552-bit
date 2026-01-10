476bfc1b888aa7308d8d2a9d6065d36a451a247a
Bharath552-bit <patthembharath32@gmail.com>
2026-01-10 20:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-//your JS code here. If required.
-// const head=document.querySelector("h1");
-// window.addEventListener('resize',()=>{
-// 	head.innerHTML=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
-// })
+const head = document.querySelector("h1");
 
-const sizeInfo = document.getElementById('sizeInfo').querySelector('h1');
-
-// Function to update the dimensions
 function updateSize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    sizeInfo.textContent = `Width: ${width} and Height: ${height}`;
+	head.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 }
 
-// Add event listener for resize
-window.addEventListener('resize', updateSize);
-
-// Initial call to display the size on page load
 updateSize();
+window.addEventListener("resize", updateSize);
